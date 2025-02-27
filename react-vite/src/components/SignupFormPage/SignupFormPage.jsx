a43241375a7af3328268ef0eee3d0ae385e743e3
Andrew <andrew.lizon12@gmail.com>
2025-02-26 17:50:00 -0800

    Finalized styling on signup

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -46,54 +46,58 @@ function SignupFormPage() {
       <h2>Sign Up</h2>
       {errors.server && <p>{errors.server}</p>}
       <form onSubmit={handleSubmit}>
-        <div className="signup_inputs">
-          <div className="signup_email_container">
-            <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            {errors.email && <p>{errors.email}</p>}
+        <div className="signup_content_container">
+          <div className="signup_inputs">
+            <div className="signup_email_container">
+              <label>Email</label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              {errors.email && <p>{errors.email}</p>}
+            </div>
+
+            <div className="signup_username_container">
+              <label>Username</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              {errors.username && <p>{errors.username}</p>}
+            </div>
+
+            <div className="signup_password_container">
+              <label>Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              {errors.password && <p>{errors.password}</p>}
+            </div>
+              
+            <div className="signup_confirm_password_container">
+              <label>Confirm Password</label>
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+            </div>
+          </div>
+          <div className="signup_button_container">
+            <button type="submit">Sign Up</button>
           </div>
 
-          <div className="signup_username_container">
-            <label>Username</label>
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            {errors.username && <p>{errors.username}</p>}
-          </div>
+        </div>
 
-          <div className="signup_password_container">
-            <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            {errors.password && <p>{errors.password}</p>}
-          </div>
-            
-          <div className="signup_confirm_password_container">
-            <label>Confirm Password</label>
-              <input
-                type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-          </div>
-        </div>
-        <div className="signup_button_container">
-          <button type="submit">Sign Up</button>
-        </div>
       </form>
     </div>
   );
