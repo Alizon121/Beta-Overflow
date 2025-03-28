bb41c69889008b25149687525550e6521e35eac0
Andrew <andrew.lizon12@gmail.com>
2025-03-28 11:32:58 -0700

    Remove question functionallity added to SavedQuestionPage, added SavedQuestionMenu component, and added styling

--- react-vite/src/components/SavedQuestionsPage/SavedQuestionsPage.jsx
+++ react-vite/src/components/SavedQuestionsPage/SavedQuestionsPage.jsx
@@ -2,10 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { thunkLoadSavedQuestions } from "../../redux/savedQuestion";
 import { thunkLoadQuestionsWithContent } from "../../redux/question";
+import SavedQuestionMenu from "../SavedQuestionMenu/SavedQuestionMenu";
+import "./SavedQuestions.css"
 
 function SavedQuestionsPage() {
     const user = useSelector(state => state?.session?.user)
-    const savedQuestions = useSelector(state => state?.savedQuestions?.savedQuestions)
+    const savedQuestions = useSelector(state => state?.savedQuestions?.savedQuestionsPaginated)
     const question = useSelector(state => state?.questions?.question)
     const [page, setPage] = useState(1)
     const [disabled, setDisabled] = useState(false)
@@ -18,7 +20,7 @@ function SavedQuestionsPage() {
 
     useEffect(() => {
         const fetchData = async () => {        
-            const response = await fetch(`/api/questions/${page+1}`)        
+            const response = await fetch(`/api/saved-questions/${page+1}`)        
             if (response.status !== 404) {
                 await dispatch(thunkLoadSavedQuestions(page));
             } else {
@@ -27,6 +29,10 @@ function SavedQuestionsPage() {
         };
         fetchData();
     }, [dispatch, page]);
+
+    const onDelete = () => {
+        dispatch(thunkLoadSavedQuestions(page))
+    }
 
     const handleNextPage = () => {
         setPage(prevPage => prevPage + 1);
@@ -38,17 +44,22 @@ function SavedQuestionsPage() {
     };
 
     return (
-        <div>
+        <div className="saved_question_page_container">
             <h2>{user.first_name}'s Saved Questions</h2>
             <div className="saved_questions_container">
                 {/* We need to use the question ids to render the questions */}
                 {/* We may need to create a new state variable to render all questions */}
                 {savedQuestions?.map(savedQuestion => (
                     question?.filter(q => q.id === savedQuestion.question_id).map(filteredQuestion => (
-                        <div key={filteredQuestion.id}>
+                        <div key={filteredQuestion.id} className="saved_question">
                             {/* Add the save button icon here */}
-                            <h3>{filteredQuestion.title}</h3>
-                            <p>{filteredQuestion.question_text}</p>
+                            <div className="saved_question_titles">
+                                <li id="saved_question_main_title">{filteredQuestion.title}</li>
+                                <li>
+                                    <SavedQuestionMenu id={filteredQuestion.id} onDelete={onDelete}/>
+                                </li>
+                            </div>
+                            <li>{filteredQuestion.question_text}</li>
                         </div>
                     ))
                 ))}
