4af7f588047f1d8ce78838fb619e30fa1af2377f
Andrew <andrew.lizon12@gmail.com>
2025-03-29 12:05:43 -0700

    Added tables to __init__.py in models folder

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
+from .comments import Comment
+from .question import Question
+from .saved_question import SavedQuestion
 from .db import environment, SCHEMA
